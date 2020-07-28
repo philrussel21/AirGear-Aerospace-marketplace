fe8d44f8238e7e5665159df8daf4c02d991d8321
philrussel21 <phil.antiporda21@gmail.com>
2020-07-28 13:59:49 +0930

    Redirects user after sign up to create profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :profiles
   root 'pages#index'
+  resources :profiles, except: [:destroy, :index]
   devise_for :accounts, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
-    sign_up: 'register'}
+    sign_up: 'register'}, controllers: {registrations: "registrations"}
+
+    # match '/user' => "welcome#index", :as => :user_root
   # get 'pages/errors'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
