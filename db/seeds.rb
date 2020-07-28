ea8caacb2eceec3abe89d9ff315b203661d560ab
philrussel21 <phil.antiporda21@gmail.com>
2020-07-28 18:18:52 +0930

    Populates database with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# populate Category Table
 categories = ["Avionics",
-              "Parts",
+              "Aeroparts",
               "Consumables",
               "Training",
               "Safety",
@@ -15,4 +17,21 @@ categories = ["Avionics",
 
 categories.each do |category|
   Category.create(name: category)
+end
+
+
+# populate Accounts Table
+10.times do |i|
+  a = Account.create(company_name: "Test Company #{i+1}", email: "test#{i+1}@email", password: "password123")
+  a.profile = Profile.create
+  sample = [1,2,3,4,5].sample
+  category = Category.all
+  conditions = Listing.conditions.keys
+  a.listings.create(category: category[sample],
+                part_num: i,
+                part_name: "PartName#{i+1}",
+                selling_price: "100#{i}",
+                currency: "AUD",
+                condition: sample
+              )
 end
