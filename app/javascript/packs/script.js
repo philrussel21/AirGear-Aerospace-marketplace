8528ebeaa616ab6112fd3a8de38ddb207abd069d
philrussel21 <phil.antiporda21@gmail.com>
2020-07-31 22:40:58 +0930

    Adds fix to navbar design

--- app/javascript/packs/script.js
+++ app/javascript/packs/script.js
@@ -1,10 +1,25 @@
+// search icon
 const search = document.querySelector("#find");
 const search_form = document.querySelector("#search");
 
+// menu icon
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+// search icon
 search.addEventListener("click", (e) => {
     if (search_form.classList.contains("hidden")) {
         search_form.classList.remove("hidden");
     } else {
         search_form.classList.add("hidden");
+    }
+});
+
+// menu icon
+burger.addEventListener("click", (e) => {
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden");
+    } else {
+        menu.classList.add("hidden");
     }
 });
---
+++ app/javascript/stylesheets/tailwindcss-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#BE0000",
+                secondary: "#323232",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
