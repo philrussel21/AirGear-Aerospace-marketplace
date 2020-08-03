ccf6ac0f01b5ec1590de883acf32dd7608903bc5
philrussel21 <phil.antiporda21@gmail.com>
2020-08-03 20:00:41 +0930

    designs & adds pagination to categories#show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.includes(listings: :account).find_by(name: params[:name])
+    @pagy, @listings = pagy(@category.listings, items: 12)
   end
 end
